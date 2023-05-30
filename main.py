7a12f36ebd4c7405511e073b49351c2ca0070590
HasanKhan04 <hasanw.khan04@gmail.com>
2023-05-30 08:32:01 -0400

    update file organization

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from forms import RegisterForm, LoginForm, ContactForm
 from sqlalchemy.ext.declarative import declarative_base
-
-from data_manager import DataManager
-from flight_search import FlightSearch
-from notification_manager import NotificationManager
-import datetime
 import os
 
 app = Flask(__name__)
@@ -127,40 +122,6 @@ def contact():
         db.session.commit()
         return redirect(url_for('contact'))
     return render_template("contact.html", form=form)
-
-
-FROM = "YYZ"
-data_manager = DataManager()
-flight_search = FlightSearch()
-notification_manager = NotificationManager()
-sheet_data = data_manager.get_destination_data()
-with app.app_context():
-    all_emails = [row.email for row in User.query.all()]
-
-
-for i in range(len(sheet_data)):
-    if sheet_data[i]['iataCode'] == "":
-        for j in range(len(sheet_data)):
-            sheet_data[j]['iataCode'] = flight_search.get_destination_code(sheet_data[j]["city"])
-            data_manager.destination_data = sheet_data
-        data_manager.update_destination_codes()
-
-tomorrow = datetime.datetime.now() + datetime.timedelta(1)
-six_months = tomorrow + datetime.timedelta(180)
-tomorrow = tomorrow.strftime("%d/%m/%Y")
-six_months = six_months.strftime("%d/%m/%Y")
-
-for city in sheet_data:
-    flight_info = flight_search.search_flights(FROM, city['iataCode'], tomorrow, six_months)
-    if flight_info is None:
-        continue
-    if city['lowestPrice'] > flight_info.price:
-        message = f"Low price alert! Only ${flight_info.price} to fly from {flight_info.from_city}-" \
-                  f"{flight_info.from_airport} to {flight_info.to_city}-{flight_info.to_airport}, from {flight_info.from_date} to {flight_info.to_date}."
-        if flight_info.stop_overs > 1:
-            message += f"\nFlight has {flight_info.stop_overs} stopover, via {flight_info.via_city}"
-        notification_manager.send_notification(message)
-        notification_manager.send_emails(message, flight_info, all_emails)
 
 
 if __name__ == "__main__":
